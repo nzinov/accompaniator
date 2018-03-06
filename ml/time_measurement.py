50eabcc09850ff0bd6d4835f7fe522943a7c52fa
Сергей Ефимочкин <Sergeyanton2007@gmail.com>
2018-03-06 03:47:27 +0300

    Have written the Classifier class and two helper classes for predicting the songs time signature

--- ml/time_measurement.py
+++ ml/time_measurement.py
@@ -1,147 +1,253 @@
+from collections import defaultdict
+
 from ml.structures import *
 
+"""
+class Track_time_signature_analyzer:
+    def __init__(self, track, time_signature_guess, bonus_for_full_length,
+                 bonus_for_same_first_chords, bonus_for_max_velocity, bonus_for_both):
+        self.track = track
+        self.bar_length = int(128 * time_signature_guess)
+        self.bonus_for_full_length = bonus_for_full_length
+        self.bonus_for_same_first_chords = bonus_for_same_first_chords
+        self.bonus_for_max_velocity = bonus_for_max_velocity
+        self.bonus_for_both = bonus_for_both
+        self.full_length = 0
 
-def find_entries(main_chords, sub_chords):
-    """
-    Finds entries of sub_chords in main_chords.
-
-    Parameteres
-    -----------
-        main_chords: a list of Chord objects to find entries in.
-
-        sub_chords: a smaller list of Chord objects.
-
-    Returns
-    -------
-        entries: a list of tuples (start, end), where
-        start is the starting point of an entry(included) and
-        end is the end of the entry(not included).
-    """
-    entries = []
-    for i in range(len(main_chords)):
-        is_equal = 1
-        j = 0
-        for main_chord, sub_chord in zip(main_chords[i: i + len(sub_chords)], sub_chords):
-            if str(main_chord) != str(sub_chord):
-                is_equal = 0
-                break
-            j += 1
-        if is_equal and j == len(sub_chords):
-            entries.append((i, i + j))
-            i += len(sub_chords)
-    return entries
-
-
-def get_check_chords(chords, bar_length):
-    """
-    Finds a Chord  sequence of given length.
-
-    Parameteres
-    -----------
-        chords: a list of Chords to find a continuing
-        Chord sequence in.
-
-        bar_length: the musical length of needed Chord
-        sequence(in 1/128ths).
-
-    Returns
-    -------
-        check_chords: a list of Chord objects of given musical length.
-    """
-    bar_sum = 0
-    check_chords = []
-    for chord in chords:
-        bar_sum += chord.duration
-        check_chords.append(chord)
-        if bar_sum == bar_length:
-            break
-        elif bar_sum > bar_length:
-            check_chords = None
-            break
-    return check_chords
+        
+        best_indent = 0
+        best_score = 0
+        for indent_base in range(int(128 / 8)):
+            indent = indent_base * 8
+            TABI = self.Track_time_signature_analyzer_by_indent(indent, track, time_signature_guess,
+                                                                bonus_for_full_length, bonus_for_same_first_chords,
+                                                                bonus_for_max_velocity, bonus_for_both)
+            if TABI.full_length and indent_base == 0:
+                self.full_length = 1
+            print(f'indent: {indent}, score: {TABI.get_score()}')
+            if TABI.get_score() > best_score:
+                best_score = TABI.get_score()
+                best_indent = indent
+        self.score = best_score
+    def get_score(self):
+        return self.score + self.full_length * self.bonus_for_full_length
+        """
 
 
-def check_time_signature(track, bar_length):
-    """
-    Gets the number of repeats of different Chord sequences
-    of certain length.
+class TrackTimeSignatureAnalyzer:
+    class Bar:
+        def __init__(self, bar_chords, duration, continued_long_note=False):
+            self.bar_chords = bar_chords
+            self.continued_long_note = continued_long_note
+            self.duration = duration
+            self.max_velocity = self.find_max_velocity(self.bar_chords)
+            self.first_chord = bar_chords[0]
 
-    Parameters
-    ----------
-        track: the Track object to check.
+        def len(self):
+            return self.duration
 
-        bar_length: the musical length of all Chord sequences
-        in question.
+        @staticmethod
+        def find_max_velocity(bar_chords):
+            max_velocity = 0
+            for chord in bar_chords:
+                if chord.velocity > max_velocity:
+                    max_velocity = chord.velocity
+            return max_velocity
 
-    Returns
-    -------
-        num_of_appearances: the number of repeats.
-    """
-    bar_length = bar_length * 128
-    num_of_appearances = 0
-    i = 0
-    while i < len(track.chords):
-        check_chords = get_check_chords(track.chords[i:], bar_length)
-        if check_chords == None:
+    def __init__(self, track, bar_length, bonus_for_full_length,
+                 bonus_for_same_first_chords, bonus_for_max_velocity, bonus_for_both):
+        # self.indent = indent
+        self.track = track
+        self.bar_length = bar_length
+        # self.indent = indent
+        self.bonus_for_full_length = bonus_for_full_length
+        self.bonus_for_same_first_chords = bonus_for_same_first_chords
+        self.bonus_for_max_velocity = bonus_for_max_velocity
+        self.bonus_for_both = bonus_for_both
+        self.full_length = 0
+        # self.cut_chords_by_indent()
+        self.bars = self.devide_track_by_bars()
+        self.score = self.count_track_score()
+
+    def get_score(self):
+        return self.score / len(self.bars) + self.full_length * self.bonus_for_full_length
+
+    def print_bars(self):
+        i = 0
+        for bar in self.bars:
+            print(f'bar #{i}, continued: {bar.continued_long_note}, velocity: {bar.max_velocity}')
+            for chord in bar.bar_chords:
+                print(str(chord))
             i += 1
-            continue
-        entries = find_entries(track.chords, check_chords)
-        num_of_appearances += len(entries) - 1  # substract 1 because the
-        # find_entries function will count the check_chords too.
-        i += len(check_chords)
-    return num_of_appearances
 
-
-def find_time_signature(track):
     """
-    CAUTION, WILL MOST CERTAINLY WORK NOT CORRECTLY(at this time).
-    Gets the time signature of the track.
-
-    Parameters
-    ----------
-        track: a track object to find time signature of.
-
-    Returns
-    -------
-        output: string with the time signature.
+    def cut_chords_by_indent(self):
+        indent_left = self.indent
+    
+        for chord in self.track.chords:
+            if indent_left == 0:
+                break
+            if chord.duration <= indent_left:
+                self.track.chords = self.track.chords[1:]
+                indent_left -= chord.duration
+            else:
+                new_chord = Chord(chord.notes, chord.duration - indent_left, chord.velocity)
+                self.track.chords = self.track.chords[1:]
+                self.track.chords.insert(0, new_chord)
+                indent_left = 0
     """
-    whole = 1.0
-    waltz = 3 / 4
-    if check_time_signature(track, whole) > 0:
-        return "4/4"
-    if check_time_signature(track, waltz) > 0:
-        return "3/4"
+
+    def devide_track_by_bars(self):
+        bars = []
+        bar_sum = 0
+        bar_chords = []
+        for chord in self.track.chords:
+            if chord.duration > (self.bar_length - bar_sum):
+                duration = chord.duration
+                if bar_sum > 0:
+                    new_chord = Chord(chord.notes, self.bar_length - bar_sum, chord.velocity)
+                    bar_chords.append(new_chord)
+                    bar = self.Bar(bar_chords, self.bar_length)
+                    bars.append(bar)
+                    bar_chords = []
+                    bar_sum = 0
+                    duration - (self.bar_length - bar_sum)
+                while duration != 0:
+                    if duration > self.bar_length:
+                        new_chord = Chord(chord.notes, self.bar_length, chord.velocity)
+                        bar_chords.append(new_chord)
+                        bar = self.Bar(bar_chords, self.bar_length, True)
+                        bars.append(bar)
+                        bar_chords = []
+                        bar_sum = 0
+                        duration -= self.bar_length
+                    else:
+                        new_chord = Chord(chord.notes, duration, chord.velocity)
+                        bar_chords.append(new_chord)
+                        bar_sum = duration
+                        duration = 0
+                continue
+            bar_sum += chord.duration
+            bar_chords.append(chord)
+            if bar_sum == self.bar_length:
+                bar = self.Bar(bar_chords, self.bar_length)
+                bars.append(bar)
+                bar_chords = []
+                bar_sum = 0
+        if bar_sum > 0:
+            bar = self.Bar(bar_chords, bar_sum)
+            bars.append(bar)
+        else:
+            self.full_length = 1
+        return bars
+
+    def count_track_score(self):
+        previous_first_bar_chord = self.bars[0].first_chord
+        score = 0
+        for bar in self.bars[1:]:
+            if not bar.continued_long_note:
+                if str(bar.first_chord) == str(previous_first_bar_chord):
+                    score += self.bonus_for_same_first_chords
+                    if bar.first_chord.velocity == bar.max_velocity:
+                        score += self.bonus_for_both
+                if bar.first_chord.velocity == bar.max_velocity:
+                    score += self.bonus_for_max_velocity
+            previous_first_bar_chord = bar.first_chord
+        return score
 
 
-"""
+class SongTimeSignatureAnalyzer:
+    def __init__(self, song, time_signature_guess, bonus_for_full_length,
+                 bonus_for_same_first_chords, bonus_for_max_velocity, bonus_for_both, group_coefficients):
+        self.song = song
+        self.bar_length = int(128 * time_signature_guess)
+        self.bonus_for_full_length = bonus_for_full_length
+        self.bonus_for_same_first_chords = bonus_for_same_first_chords
+        self.bonus_for_max_velocity = bonus_for_max_velocity
+        self.bonus_for_both = bonus_for_both
+        self.full_length = 0
+        self.groups_coefficients = group_coefficients
 
-EXAMPLES BELOW
+        self.groups = self.divide_tracks_on_groups(song)
+        self.score = self.count_song_score()
 
-"""
+    @staticmethod
+    def divide_tracks_on_groups(song):
+        groups = {"drums": [], "bass": [], "rhythm": [], "guitar": [], "etc": []}
+        for track in song.tracks:
+            if str(track).lower().find("drums") > -1 or str(track).lower().find("percussion") > -1 or \
+                    track.program <= 0 or 9 <= track.program <= 16 or 113 <= track.program <= 120:
+                groups["drums"].append(track)
+                continue
+            elif str(track).lower().find("bass") > -1 or 33 <= track.program <= 40:
+                groups["bass"].append(track)
+                continue
+            elif str(track).lower().find("rhythm") > -1:
+                groups["rhythm"].append(track)
+                continue
+            elif str(track).lower().find("guitar") > -1 or 25 <= track.program <= 32:
+                groups["guitar"].append(track)
+                continue
+            else:
+                groups["etc"].append(track)
+                continue
+        return groups
 
-tmp_chords_whole = [[16, 0, [1, 2, 3]], [16, 0, [4, 5, 6]], [8, 0, [2, 3]],
-                         [8, 0, [2, 3]], [16, 0, [1, 2, 3]], [32, 0, [10, 11, 12]],
-                         [16, 0, [1, 2, 3]], [16, 0, [4, 5, 6]], [32, 0, [14, 15, 16]],
-                         [32, 0, [17, 18, 19]], [32, 0, [20, 21, 22]], [32, 0, [23, 24, 25]],
-                         [16, 0, [1, 2, 3]], [16, 0, [4, 5, 6]], [8, 0, [2, 3]],
-                         [8, 0, [2, 3]], [16, 0, [1, 2, 3]], [32, 0, [10, 11, 12]],
-                         [16, 0, [1, 2, 3]], [16, 0, [4, 5, 6]]]
-tmp_sub = [[16, 0, [1, 2, 3]], [16, 0, [4, 5, 6]]]
+    def count_song_score(self):
 
-chords_whole = []
-for x in tmp_chords_whole:
-    chord = Chord(x[2], x[0], x[1])
-    chords_whole.append(chord)
+        score = 0
+        for instrument in self.groups.keys():
+            coefficient = self.groups_coefficients[instrument]
+            for track in self.groups[instrument]:
+                ta = TrackTimeSignatureAnalyzer(track, self.bar_length, self.bonus_for_full_length,
+                                                self.bonus_for_same_first_chords, self.bonus_for_max_velocity,
+                                                self.bonus_for_both)
+                score += ta.get_score() * coefficient
+        return score
 
-sub = []
-for x in tmp_sub:
-    chord = Chord(x[2], x[0], x[1])
-    sub.append(chord)
+    def get_score(self):
+        return self.score
 
-entries = find_entries(chords_whole, sub)
 
-for entry in entries:
-    print(entry)
+class TimeSignatureClassifier:
+    groups_coefficients = {"drums": 1.1, "bass": 1.0, "rhythm": 0.9, "guitar": 0.5, "etc": 0.3}
+    bonus_for_full_length = 7
+    bonus_for_same_first_chords = 15
+    bonus_for_max_velocity = 20
+    bonus_for_both = 50
 
-hard_track_whole = Track(chords_whole, 'Vocals')
-print(f'time signature: {find_time_signature(hard_track_whole)}')
+    tested_time_signatures = {"3/4": 0.75, "4/4": 1.0}
+
+    def __init__(self, song):
+        self.song = song
+        self.results = self.predict()
+
+    def predict(self):
+
+        results = defaultdict(float)
+        for time_signature in self.tested_time_signatures.keys():
+            analyzer = SongTimeSignatureAnalyzer(self.song, self.tested_time_signatures[time_signature],
+                                                 self.bonus_for_full_length, self.bonus_for_same_first_chords,
+                                                 self.bonus_for_max_velocity, self.bonus_for_both,
+                                                 self.groups_coefficients)
+            score = analyzer.get_score()
+            results[time_signature] = score
+
+        return results
+
+    def print_result(self):
+        max_score = 0
+        max_time_signature = ""
+
+        for time_signature in self.results.keys():
+            if self.results[time_signature] > max_score:
+                max_score = self.results[time_signature]
+                max_time_signature = time_signature
+
+        return f'Song: {self.song.name}, Time Signature: {max_time_signature}'
+
+    def print_all_results(self):
+        ret = f'Song: {self.song.name}, results: \n'
+        for time_signature in self.results.keys():
+            ret += f'Time Signature: {time_signature}, Score: {self.results[time_signature]}\n'
+        return ret
