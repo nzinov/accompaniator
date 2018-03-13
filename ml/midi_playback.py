5426b4967cd109c21145c11ed9678f2538275148
AnnaKlimchuk <anna_klm@mail.ru>
2018-03-13 17:22:23 +0300

    creating a MIDI file to play

--- ml/midi_playback.py
+++ ml/midi_playback.py
@@ -4,10 +4,11 @@ import numpy as np
 from mido import Message, MidiFile, MidiTrack
 
 # ... data for test ...
+# IMPORTANT time is measured in milliseconds
 # TODO: detect correctness of data
 numbers = np.array([60, 60, 64, 67])
-times = np.array([3, 3, 6, 9])
-lengths = np.array([8, 4, 2, 1])
+times = np.array([3000, 3000, 6000, 9000])
+lengths = np.array([8000, 4000, 2000, 1000])
 delta = 10
 
 if __name__ == "__main__":
@@ -19,7 +20,7 @@ if __name__ == "__main__":
         mid.tracks.append(tracks[i])
         
         tracks[i].append(Message('program_change', program=33, time=delta))
-        tracks[i].append(Message('note_on', note=numbers[i], velocity=124, time=times[i] * 1000))
-        tracks[i].append(Message('note_off', note=numbers[i], velocity=0, time=lengths[i] * 1000)) 
+        tracks[i].append(Message('note_on', note=numbers[i], velocity=124, time=times[i]))
+        tracks[i].append(Message('note_off', note=numbers[i], velocity=0, time=lengths[i])) 
     
     mid.save('tmp.mid')
