40898163ffb8a990c9e41e80c2f45cb29e442d57
wiki-fan <uberslowpoke@gmail.com>
2018-04-15 14:05:30 +0300

    Docker improved

--- docker/accompaniator/run.sh
+++
@@ -1,6 +0,0 @@
-docker run -ti --rm \
-    -v /dev/snd:/dev/snd \
-    -v /tmp/.X11-unix:/tmp/.X11-unix -v /run/dbus/:/run/dbus/:rw -v /dev/shm:/dev/shm \
-    --group-add audio \
-    --privileged \
-    docker_accompaniator bash
