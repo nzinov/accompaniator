5f9c3ffb0368323363fa054f96acd392ede8bc11
sharfiky <kireevkarim@yandex.ru>
2018-05-13 14:26:41 +0300

    with input with script

---
+++ accompaniator_web/static/js/result.js
@@ -0,0 +1,18 @@
+function onDownload() {
+    var text = document.getElementsByTagName("input")[2];
+    var val=text.innerHTML;
+    var star = document.getElementsById("mark");
+}
+function one() {
+        document.getElementById("mark").innerHTML = "1";
+        alert(1);
+}
+function second() {
+        document.getElementById("mark").innerHTML = "2";
+}function third() {
+        document.getElementById("mark").innerHTML = "3";
+}function fourth() {
+        document.getElementById("mark").innerHTML = "4";
+}function five() {
+        document.getElementById("mark").innerHTML = "5";
+}
