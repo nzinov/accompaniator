b85e49dc6c942865de92ee80ab640bcbd3d7cc14
sharfiky <kireevkarim@yandex.ru>
2018-05-15 22:34:14 +0300

    bright stars

--- accompaniator_web/static/js/result.js
+++ accompaniator_web/static/js/result.js
@@ -3,16 +3,12 @@ function onDownload() {
     var val=text.innerHTML;
     var star = document.getElementsById("mark");
 }
-function one() {
-        document.getElementById("mark").innerHTML = "1";
-        alert(1);
-}
-function second() {
-        document.getElementById("mark").innerHTML = "2";
-}function third() {
-        document.getElementById("mark").innerHTML = "3";
-}function fourth() {
-        document.getElementById("mark").innerHTML = "4";
-}function five() {
-        document.getElementById("mark").innerHTML = "5";
+function star(num, star) {
+        for (var i = 1; i <= num; i++) {
+            document.getElementById("star" + i).innerHTML = '<img src="../../static/img/star.png" width="100%" height="100%">';
+        }
+        for (var i = num + 1; i <= 5; i++) {
+            document.getElementById("star" + i).innerHTML = '<img src="../../static/img/star_dark.png" width="100%" height="100%">';
+        }
+        document.getElementById("mark").innerHTML = num;
 }
