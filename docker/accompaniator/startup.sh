e68bf60081d89b79a7f4c809215e70c14796fee7
wiki-fan <uberslowpoke@gmail.com>
2018-05-19 12:08:38 +0300

    Runserver on container startup

--- docker/accompaniator/startup.sh
+++ docker/accompaniator/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Start the first process
+# Start fluidsynth
 nohup fluidsynth -a alsa -m alsa_seq -l -s -i /usr/share/soundfonts/FluidR3_GM.sf2 &
 status=$?
 if [ $status -ne 0 ]; then
@@ -8,6 +8,20 @@ if [ $status -ne 0 ]; then
   exit $status
 else
   echo "Fluidsynth ran successfully"
+fi
+
+cd accompaniator
+git checkout ACCOMPANIST-154_site_on_django
+cd accompaniator_web
+
+# Start django
+nohup python manage.py makemigrations && python manage.py migrate && python manage.py runserver 0.0.0.0:8000 &
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start django: $status"
+  exit $status
+else
+  echo "Django ran successfully"
 fi
 
 bash
