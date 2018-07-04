916d3a882e4f3f42ba3eb346f330fa8779b1cc1a
wiki-fan <uberslowpoke@gmail.com>
2018-07-04 12:57:07 +0300

    Demo of midi playing and pitch detection

---
+++ PlayWithMe/app/src/main/java/accompaniator_team/playwithme/MainActivity.java
@@ -0,0 +1,169 @@
+package accompaniator_team.playwithme;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.SystemClock;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.billthefarmer.mididriver.MidiDriver;
+
+import be.tarsos.dsp.AudioDispatcher;
+import be.tarsos.dsp.AudioEvent;
+import be.tarsos.dsp.AudioProcessor;
+import be.tarsos.dsp.io.android.AudioDispatcherFactory;
+import be.tarsos.dsp.pitch.PitchDetectionHandler;
+import be.tarsos.dsp.pitch.PitchDetectionResult;
+import be.tarsos.dsp.pitch.PitchProcessor;
+
+public class MainActivity extends AppCompatActivity {
+
+    MidiDriver midiDriver;
+    TextView noteText, pitchText;
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, REQUEST_RECORD_AUDIO_PERMISSION);
+        }
+
+        boolean hasLowLatencyFeature =
+                getPackageManager().hasSystemFeature(PackageManager.FEATURE_AUDIO_LOW_LATENCY);
+
+        boolean hasProFeature =
+                getPackageManager().hasSystemFeature(PackageManager.FEATURE_AUDIO_PRO);
+
+        noteText = findViewById(R.id.textViewNoteText);
+        pitchText = findViewById(R.id.textViewPitchText);
+        TextView hardwareSoundInfo = findViewById(R.id.textViewHardwareSoundInfo);
+        hardwareSoundInfo.setText(String.format("hasLowLatencyFeature: %b\nhasProFeature: %b",
+                hasLowLatencyFeature, hasProFeature));
+
+        midiDriver = new MidiDriver();
+
+        AudioDispatcher dispatcher = AudioDispatcherFactory.fromDefaultMicrophone(22050,1024,0);
+
+        PitchDetectionHandler pdh = new PitchDetectionHandler() {
+
+            @Override
+            public void handlePitch(PitchDetectionResult res, AudioEvent e){
+                final float pitchInHz = res.getPitch();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        processPitch(pitchInHz);
+                    }
+                });
+            }
+        };
+
+        AudioProcessor pitchProcessor = new PitchProcessor(PitchProcessor.PitchEstimationAlgorithm.FFT_YIN, 22050, 1024, pdh);
+        dispatcher.addAudioProcessor(pitchProcessor);
+
+        Thread audioThread = new Thread(dispatcher, "Audio Thread");
+        audioThread.start();
+    }
+
+    public void processPitch(float pitchInHz) {
+
+        pitchText.setText("" + pitchInHz);
+
+        if(pitchInHz >= 110 && pitchInHz < 123.47) {
+            //A
+            noteText.setText("A");
+        }
+        else if(pitchInHz >= 123.47 && pitchInHz < 130.81) {
+            //B
+            noteText.setText("B");
+        }
+        else if(pitchInHz >= 130.81 && pitchInHz < 146.83) {
+            //C
+            noteText.setText("C");
+        }
+        else if(pitchInHz >= 146.83 && pitchInHz < 164.81) {
+            //D
+            noteText.setText("D");
+        }
+        else if(pitchInHz >= 164.81 && pitchInHz <= 174.61) {
+            //E
+            noteText.setText("E");
+        }
+        else if(pitchInHz >= 174.61 && pitchInHz < 185) {
+            //F
+            noteText.setText("F");
+        }
+        else if(pitchInHz >= 185 && pitchInHz < 196) {
+            //G
+            noteText.setText("G");
+        }
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+
+        // Start midi
+
+        if (midiDriver != null)
+            midiDriver.start();
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+
+        // Stop midi
+
+        if (midiDriver != null)
+            midiDriver.stop();
+
+    }
+
+    public void playTestSound(View view) {
+        TextView info = (TextView)findViewById(R.id.textViewSoundInfo);
+        info.setText("Playing");
+        sendMidi(0xC0, 79);
+        sendMidi(0x90, 48, 63);
+        sendMidi(0x90, 52, 63);
+        sendMidi(0x90, 55, 63);
+        SystemClock.sleep(1000);
+        sendMidi(0x80, 48, 0);
+        sendMidi(0x80, 52, 0);
+        sendMidi(0x80, 55, 0);
+        info.setText("Not playing");
+    }
+
+    // Send a midi message
+
+    protected void sendMidi(int m, int p) {
+        byte msg[] = new byte[2];
+
+        msg[0] = (byte) m;
+        msg[1] = (byte) p;
+
+        midiDriver.write(msg);
+    }
+
+    // Send a midi message
+
+    protected void sendMidi(int m, int n, int v) {
+        byte msg[] = new byte[3];
+
+        msg[0] = (byte) m;
+        msg[1] = (byte) n;
+        msg[2] = (byte) v;
+
+        midiDriver.write(msg);
+    }
+}
